6883b48266ac579a80ecbff614188e8615dc7b1c
Mario Ervedosa <139678911+supermarmar@users.noreply.github.com>
2025-11-15 01:05:10 +0000

    Add files via upload

---
+++ notebooks/03_feature_engineering/area/yellow_maize_area_parms.py
@@ -0,0 +1,10 @@
+config = {
+    "ABS_PATH": r"C:\Users\mario\OneDrive\Documents\Work\Clients\Agnify\\",
+    "MODEL_PATH": r"C:\Users\mario\OneDrive\Documents\Work\Clients\Agnify\3. Models\Pre-Credit Score\1. Yield Consistency\2. Data Science\Area",
+    "DATA_FILE_PATH": r"C:\Users\mario\OneDrive\Documents\Work\Clients\Agnify\2. Data\SAFEX\Crop Estimates",
+    "DATA_FILE": "safex_areas.parquet",
+    "TARGET_VARIABLE": "Area (Hectares)",
+    "CROP": "Yellow Maize",
+    "MODEL_FILE": "yellow_maize_province_areas",
+    "YEAR": 2024,
+}
